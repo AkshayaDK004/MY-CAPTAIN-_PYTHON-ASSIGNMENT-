09d072feb943ca50e46a0e429061bb9a9bb3938b
AkshayaDK004 <128928506+AkshayaDK004@users.noreply.github.com>
2023-03-26 00:14:28 +0530

    Created Ext.py
    A python program to print the extension for the given file name.

---
+++ Ext.py
@@ -0,0 +1,13 @@
+filename=input()
+if '.py' in filename:
+	print("The extension is python")
+elif '.c' in filename:
+	print("The extension is c")
+elif '.cpp' in filename:
+	print("The extension is c++")
+elif '.java' in filename:
+	print("The extension is java")
+elif '.js' in filename:
+	print("The extension is javascript")
+else:
+	print("no information")
